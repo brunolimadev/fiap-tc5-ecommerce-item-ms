bd911fa869d8fd0ab4d0ae0e7d25deb759d63a83
Wiliam Nascimento <wilnasc990s3@gmail.com>
2024-07-01 19:46:41 -0300

    Adicionando estrutura inicial do projeto

---
+++ src/main/java/br/com/fiap/ecommerce_item_ms/EcommerceItemMsApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.ecommerce_item_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceItemMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceItemMsApplication.class, args);
+	}
+
+}
