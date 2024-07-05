5f3aaf83f969786c74ac724a34f5a24a9f95b96d
Wiliam Nascimento <wilnasc990s3@gmail.com>
2024-07-04 23:44:00 -0300

    Testando as entidades de negócio e o controller advice

---
+++ src/test/java/br/com/fiap/ecommerce_item_ms/domain/entities/MessageEntityTest.java
@@ -0,0 +1,22 @@
+package br.com.fiap.ecommerce_item_ms.domain.entities;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MessageEntityTest {
+
+  @Test
+  void shouldCreateMessageEntity() {
+
+    assertThat(MessageEntity
+            .builder()
+            .title("Título")
+            .message("Mensagem")
+            .build()
+    )
+            .isNotNull();
+
+  }
+
+}
