56607ccc94e746e961abc2ee26eb4acb732f6077
Bruno <bruno.lima.dev@outlook.com>
2024-07-20 08:04:02 -0300

    chore: added session host url

--- src/main/java/br/com/fiap/ecommerce_item_ms/adapter/config/PortsConfig.java
+++ src/main/java/br/com/fiap/ecommerce_item_ms/adapter/config/PortsConfig.java
@@ -5,23 +5,28 @@ import br.com.fiap.ecommerce_item_ms.ports.outputport.ItemManagementOutputPort;
 import br.com.fiap.ecommerce_item_ms.adapter.ports.outputport.ItemManagementOutputPortAdapter;
 import br.com.fiap.ecommerce_item_ms.adapter.repositories.ItemRepository;
 import br.com.fiap.ecommerce_item_ms.ports.outputport.SessionManagementOutputPort;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class PortsConfig {
 
+  @Value("${application.session.host-url}")
+  private String sessionServiceHost;
+
   @Bean
   public SessionManagementOutputPort sessionManagementOutputPort() {
 
-    return new SessionManagementOutputPortAdapter();
+    return new SessionManagementOutputPortAdapter(sessionServiceHost);
 
   }
 
   @Bean
   public ItemManagementOutputPort itemManagementOutputPort(
-          ItemRepository itemRepository,
-          SessionManagementOutputPort sessionManagementOutputPort) {
+      ItemRepository itemRepository,
+      SessionManagementOutputPort sessionManagementOutputPort) {
 
     return new ItemManagementOutputPortAdapter(itemRepository, sessionManagementOutputPort);
 
